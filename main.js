60eb50ed5dd30da8c6c5408911a1a532d25e437a
KvM <k044269725@gmail.com>
2023-10-22 15:27:55 +0200

    started working with main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+let valueDisplay;
+let valueString;
+let valueNumbResult;
+let calcOne;
+let calcTwo;
+let calcThree;
+let calcFour;
+let calcFive;
+let calcSix;
+let calcSeven;
+let calcEight;
+let calcNine;
+let calcZero;
+let calcReset;
+let calcPosNeg;
+let calcPercentage;
+let calcDivide;
+let calcMultiply;
+let calcSubstract;
+let calcAdd;
+let calcComma;
+let calcEqual;
+
+const main = () => {
+
+}
+
+const prepareDOMElements = () => {
+
+}
+
+const prepareDOMEvents = () => {
+
+}
+
+document.addEventListener('DOMContentLoaded', main)
